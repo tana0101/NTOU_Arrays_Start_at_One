a296f341752fc10ee47b6e0bbb05df7feaab8141
tana0101 <98708317+tana0101@users.noreply.github.com>
2022-05-14 19:52:38 +0800

    Create AC_2.cpp

---
+++ content/AC_2.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+#define INF 0x3f3f3f3f
+#define MOD 1000000007
+#define ll  long long
+#define ld  long double
+#define pb  push_back
+#define ppb pop_back
+#define pf  push_front
+#define ppf pop_front
+#define ep  emplace
+#define epb emplace_back
+#define all(x) x.begin(),x.end()
+#define io  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+//#define int long long
+using namespace std;
+
+bool cmp(int a,int b){
+    return a > b;
+}
+
+signed main() {
+    io
+    ll t,n,m,i,j,k,temp;
+    int a,b,c,d,e,f,g,h;
+    cin >>
+
+    
+
+}
