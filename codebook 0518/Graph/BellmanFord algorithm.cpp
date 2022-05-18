6c10b1b8240b705d4a921d5f85407d754ec9d542
tana0101 <98708317+tana0101@users.noreply.github.com>
2022-05-18 16:01:13 +0800

    Add files via upload

--- codebook 0518/Graph/BellmanFord algorithm.cpp
+++ codebook 0518/Graph/BellmanFord algorithm.cpp
@@ -6,12 +6,10 @@ void BellmanFord(int n, int v)
 { /* n為節點總數，計算單一起點v/所有終點的最短路徑，其中邊長允許是負值，length為adjacency matrix */
     for (int k = 0; k < n; k++)for (int i = 0; i < n; i++)i == 0 ? dist[k][i] = 0 : dist[k][i] = INF; /* 對dist做初始化 */
     for (int i = 0; i < n; i++)if(length[v][i])dist[1][i] = length[v][i]; /* 對dist[1]做初始化 */
-    
     for (int i = 0; i < n; i++) {
         dist[1][i] == INF ? cout << "i" : cout << dist[1][i];
         if (i != n - 1)cout << " ";
     }cout << "\n";
-
     for (int k = 2; k <= n - 1; k++) {
         for (int u = 0; u < n; u++) {
             for (int i = 0; i < length[u].size(); i++) {
